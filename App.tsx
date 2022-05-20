a7f9cc520871d98f2b21fafe91e9f9fc8cc0f259
mohandand <mohandand@gmail.com>
2022-05-20 13:54:29 -0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see somsdsdse magic happen :)</p>
+      <p>Start editing to see somsdsdse magsasaic happen :)</p>
     </div>
   );
 }
