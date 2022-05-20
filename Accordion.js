2e96b01f6413f7a6074b99a1ae4a3037869e0c1d
mohandand <mohandand@gmail.com>
2022-05-20 13:59:41 -0700

    1 file modified, 2 added, 1 deleted

---
+++ Accordion.js
@@ -0,0 +1,30 @@
+import React, { useState, useRef } from "react";
+import "./styles.css";
+
+export default function Accordion(props) {
+  const [height, setHeight] = useState("0px");
+  const [arrow, setArrow] = useState("sarrow");
+  const [active, setActive] = useState("accrdion");
+
+  let content = useRef(null);
+  function toggleContent() {
+    setHeight(height === "0px" ? `${content.current.scrollHeight}px` : "0px");
+    setArrow(height === "0px" ? "darrow" : "sarrow");
+    setActive(height === "0px" ? "accrdion active" : "accrdion");
+  }
+  return (
+    <div className="accrdion_section">
+      <button className={active} onClick={toggleContent}>
+        <p>{props.title}</p>
+        <span className={arrow}></span>
+      </button>
+      <div
+        ref={content}
+        style={{ maxHeight: `${height}` }}
+        className="accordion_content"
+      >
+        {props.content}
+      </div>
+    </div>
+  );
+}
--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
-import './style.css';
+import './styles.css';
+import Accordion from './Accordion';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see somsdsdse magsasaic happen :)</p>
+    <div className="App">
+      <div>
+        <h2>Accordion</h2>
+      </div>
+      <Accordion
+        title={'Title of The Accrdion-1 '}
+        content={'The coneten of Accrdion is funny'}
+      />
+      <Accordion
+        title={'Title of The Accrdion-1'}
+        content={
+          'The coneten of Accrdion is funny Online Grammar and Writing Checker To Help You Deliver Impeccable, Mistake-free Writing. Grammarly Has a Tool For Just About Every Kind Of Writing You Do. Try It Out For Yourself! Easily Improve Any Text. Fix Punctuation Errors. AI Writing Assistant. Improve Word Choice.'
+        }
+      />
+      <Accordion
+        title={'Title of The Accrdion-1'}
+        content={'The coneten of Accrdion is funny'}
+      />
     </div>
   );
 }
