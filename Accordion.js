d3d6a7c24161d6c26c01b254bd83c456aaf1c41a
mohandand <mohandand@gmail.com>
2023-01-29 18:42:30 -0600

    2 files modified

--- Accordion.js
+++ Accordion.js
@@ -2,6 +2,7 @@ import React, { useState, useRef } from "react";
 import "./styles.css";
 
 export default function Accordion(props) {
+
   const [height, setHeight] = useState("0px"); //Based on height of content we will change arrow direction and change content display
   const [arrow, setArrow] = useState("sarrow");
   const [active, setActive] = useState("accrdion");
@@ -14,6 +15,7 @@ export default function Accordion(props) {
     setArrow(height === "0px" ? "darrow" : "sarrow");
     setActive(height === "0px" ? "accrdion active" : "accrdion");
   }
+
   return (
     <div className="accrdion_section">
       <button className={active} onClick={toggleContent}> 
@@ -22,7 +24,7 @@ export default function Accordion(props) {
       </button>
       <div
         ref={content} //refernce to this div
-        style={{ maxHeight: `${height}` }} //JSXx inline style to cahnge height
+        style={{ maxHeight: `${height}` }} //JSXx inline style to change height
         className="accordion_content"
       >
         {props.content}
